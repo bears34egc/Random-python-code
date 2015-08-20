9ed9fa94fa0addf9b4fa4b07f95c8479c3101c01
Eric Coker <bears34egc@yahoo.com>
2015-08-20 00:04:18 -0500

    Create mysql_pandas.py

---
+++ mysql_pandas.py
@@ -0,0 +1,81 @@
+import json
+import urllib
+import requests
+import urllib2
+from nltk.misc import babelfish
+import unicodedata
+import pandas.io.sql
+import pyodbc
+import pandas as pd
+from sqlalchemy import create_engine, MetaData, Table, select, String
+import sys
+import datetime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+def safe_str(obj):
+    """ return the byte string representation of obj """
+    try:
+        return str(obj)
+    except UnicodeEncodeError:
+        # obj is unicode
+        return unicode(obj).encode('unicode_escape')
+
+try:
+    server = "SQLEXPRESS"
+    db = "TABLEAU"
+
+    engine=create_engine("mssql+pyodbc://ODBCDriver/")
+    Base.metadata.create_all(engine)
+    connection=engine.connect()
+    trans=connection.begin()
+    try:
+    	r1=connection.execute("[dbo].[CSAT_NSAT_pull]")
+    	trans.commit()
+    except:
+    	trans.rollback()
+        print 'ISSUE!'
+    	raise
+    connection.close()
+    print 'stored procedure complete'
+
+    df=pd.read_sql_table("CHATS",engine,index_col=None,coerce_float=True,parse_dates=['CHAT_REQUEST_DATE'])
+    print 'data read'
+    print df.info()
+    print len(df)
+    df = df.where((pd.notnull(df)), None)
+    engine.dispose()
+
+
+    engine=create_engine("mysql+mysqldb://username:password@localhost:3306/schema")
+    df.to_sql(schema='schema',name='chats', con=engine,if_exists='replace', index=False,chunksize=100000)
+    print 'incr data transferred to mysql'
+
+    print 'complete'
+
+except Exception,err:
+    import traceback
+    import sys
+    import smtplib
+    import datetime
+    import time
+    e=str(traceback.format_exc())
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    fromMy = 'name@yahoo.com'
+    to = 'name@outlook.com'
+    subj='mysql_pandas.py'
+    date=str(st)
+    message_text=date + e
+    msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % ( fromMy, to, subj, date, message_text )
+
+    username=str('username')
+    password=str('password')
+
+    server=smtplib.SMTP("smtp.mail.yahoo.com",587)
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(fromMy,to,msg)
+    server.quit()
+    print 'ok the email has sent'
